59df0a579f443b419b32618d339951865a54d057
SRAKIB17 <rakibulssc5@gmail.com>
2025-05-06 23:11:01 +0600

    done

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,12 @@ const app = new TezX({ debugMode: true });
 app.use([logger()]);
 
 app.static("/", "./static");
-
+app.use(async (ctx, next) => {
+    // await new Promise((resolve) => setTimeout(resolve, 6000)); // 6 seconds
+    await next()
+})
 app.get("/", (ctx) => {
+
     return ctx.redirect("/index.html");
 });
 
